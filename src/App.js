10ea68583745899deb393a5e65f08ce68f8de221
Matt Long <matthewhl@gmail.com>
2018-04-19 16:44:15 -0400

    some cleanup

--- src/App.js
+++ src/App.js
@@ -12,10 +12,10 @@ const API_URL = `http://api.aerisapi.com/forecasts/11101?client_id=${CLIENT_ID}&
 
 class App extends Component {
   state = {
-    todaysForecast: null,
     weekForecast: [],
     showCelsius: false,
-    loading: true
+    loading: true,
+    errorResponse: null
   };
   
   switchTemperatureType() {
@@ -23,31 +23,47 @@ class App extends Component {
   }
 
   componentWillMount() {
-    fetch(API_URL).then(response => response.json()).then(data => {
+    fetch(API_URL).then(response => {
+      if (response.ok) {
+        return response.json()  
+      } else {
+        return Promise.reject(response.statusText);
+      }
+    }).then(data => {
+      if (data.error) {
+        return this.setState({ errorResponse: data.error.description, loading: false });
+      }
       let forecastData = data.response[0];
       this.setState({
-        todaysForecast: forecastData.periods[0],
         weekForecast: forecastData.periods,
         loading: false
       });
+    }, errorResponse => {
+      this.setState({ errorResponse, loading: false });
     });
   }
 
   render() {;
-    let { todaysForecast, weekForecast, showCelsius } = this.state;
-    let weeklyForecast = weekForecast.map((forecast, idx) => {
+    let { loading, showCelsius, errorResponse } = this.state;
+    
+    let weeklyForecast = this.state.weekForecast.map((forecast, idx) => {
       return (
         <WeatherBadge key={idx} isToday={idx === 0} forecast={forecast} showCelsius={showCelsius} />
       );
     });
-
+    let content;
+    if (errorResponse && !loading) {
+      content = <div id='err'>Oops! {errorResponse}</div>;
+    } else if (loading) {
+      content = <div id='loading'>I'm a spinner! Watch me spin! *spins*</div>;
+    } else {
+      content = <div id='weekly-forecast'>{weeklyForecast}</div>;
+    }
 
     return (
       <div id="app">
         <div onClick={this.switchTemperatureType.bind(this)} id='container'>
-          <div id='weekly-forecast'>
-            {weeklyForecast}
-          </div>
+          {content}
         </div>
       </div>
     );
